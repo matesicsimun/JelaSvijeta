eaa62e1da5e6c0a10da8a2262d55426c19800646
Simun <simunm@outlook.com>
2023-09-05 09:08:26 +0200

    replace Response with more appropriate JsonResponse

--- src/Controller/MealController.php
+++ src/Controller/MealController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\MealService;
 use App\Service\ValidationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class MealController extends AbstractController
 {
     #[Route('/meals')]
-    public function getMeals(Request $request, MealService $mealService, ValidationService $validationService): Response
+    public function getMeals(Request $request, MealService $mealService, ValidationService $validationService): JsonResponse
     {
         $errors = $validationService->validate($request);
         if ($errors) {
-            return new Response(json_encode(['status' => 400, 'errors' => $errors]));
+            return new JsonResponse(['status' => 400, 'errors' => $errors]);
         } else {
-            return new Response(json_encode($mealService->getMeals($request)));
+            return new JsonResponse($mealService->getMeals($request));
         }
     }
 
